ea8a4f192ac23898d5d29ec7a08065e962b7b75a
Yufei-Chang <yufei.chang@hotmail.com>
2024-11-07 16:17:10 +0100

    feat: correzione esercizi

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,9 +5,21 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+const curItem = (nome) => {
+    const now = new Date();
+    const orario = now.getHours();
+    let message = 'Buonasera';
+    if (orario < 13 && orario > 0) {
+        message = 'Buongiorno';
+    } else if (orario >=13 && orario <= 17){
+        message = 'Buon pomeriggio';
+    }
+    return `${message} ${nome}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const messaggio = curItem(name);
+console.log(messaggio)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
